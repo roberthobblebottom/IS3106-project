8a23aca4f652ec13c42c9bbf56df4cecf79efa3e
Ng Guangren, Ryan <e0003902@u.nus.edu>
2020-02-29 13:09:23 +0800

    Added User entity
    used SerialBlob for image storage, is this feasable?

---
+++ GamingNexus/GamingNexus-ejb/src/java/ejb/entity/User.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.sql.rowset.serial.SerialBlob;
+
+/**
+ *
+ * @author root
+ */
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userID;
+    private int phoneNumber;
+    private String address;
+    private String email;
+    private String username;
+    private String password;
+    private SerialBlob profilePicture;//https://stackoverflow.com/questions/29208007/what-is-the-data-type-for-images-in-java
+    private Date lastOnline;
+    //TODO: relational mapping
+    
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userID != null ? userID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the userID fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.userID == null && other.userID != null) || (this.userID != null && !this.userID.equals(other.userID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ejb.entity.User[ id=" + userID + " ]";
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the profilePicture
+     */
+    public SerialBlob getProfilePicture() {
+        return profilePicture;
+    }
+
+    /**
+     * @param profilePicture the profilePicture to set
+     */
+    public void setProfilePicture(SerialBlob profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    /**
+     * @return the lastOnline
+     */
+    public Date getLastOnline() {
+        return lastOnline;
+    }
+
+    /**
+     * @param lastOnline the lastOnline to set
+     */
+    public void setLastOnline(Date lastOnline) {
+        this.lastOnline = lastOnline;
+    }
+    
+}
